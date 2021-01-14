d7efc3168bbcd69b5e0a92020cc30da6f21a93ea
fandyfr <fandlyfr@gmail.come.com>
2021-01-14 10:14:27 +0800

    add mysql connector, make controller

--- src/main/java/my/id/fanfan/jwtimplementation/controller/UserController.java
+++ src/main/java/my/id/fanfan/jwtimplementation/controller/UserController.java
@@ -1,4 +1,27 @@
 package my.id.fanfan.jwtimplementation.controller;
 
+import my.id.fanfan.jwtimplementation.entity.User;
+import my.id.fanfan.jwtimplementation.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    private UserRepository userRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserController(UserRepository userRepository,BCryptPasswordEncoder bCryptPasswordEncoder){
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @PostMapping("/sign-up")
+    public void  signup(@RequestBody User user){
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
 }
